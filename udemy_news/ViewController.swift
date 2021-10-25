76b3452f4aa425c1407a4b9099a3835632f400d0
Denis Schüle <schueledenis@gmx.at>
2021-10-25 18:58:06 +0200

    initial

---
+++ udemy_news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  udemy_news
+//
+//  Created by Denis Schüle on 25.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
